689157b919a80224033f4cf99481416e0ad5cea5
KangYue <ky1000ky2000@gmail.com>
2014-03-28 12:31:58 +0800

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	$url = 'http://10.103.242.154:8080/SSHDemo/mytest.action';
+	//$data = array('key1' => 'value1', 'key2' => 'value2');
+	$options = array(
+	    'http' => array(
+	        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'GET',
+	        //'content' => http_build_query($data),
+	    ),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	var_dump($result);
+
+
+	$json = json_decode($result,TRUE);
+	var_dump($json);
+
+?>
